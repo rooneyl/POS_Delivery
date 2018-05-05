cb91b8120f6d215411539b311650361cc2869612
seongjun lee <rooneylee@outlook.com>
2018-05-04 18:56:30 -0700

    [INIT] enum for categories of menu items

---
+++ src/main/java/pos_delivery/model/Category.java
@@ -0,0 +1,30 @@
+package pos_delivery.model;
+
+/**
+ * Category Enum Represents Categories of Items
+ */
+public enum Category {
+    APPETIZER("Appet & Salad"),
+    ENTREE("Entree"),
+    TEMPURA("Tempura"),
+    UDON("Udon&Don"),
+    NIGIRI("Nigiri Sushi"),
+    OSHI("Oshi Sushi"),
+    SASHIMI("Sashimi"),
+    ROLL("Roll & Cone"),
+    COMBO("Combo"),
+    SPECIAL("Special Rolls");
+
+    /**
+     * Description is Used to Represent Category in UI
+     **/
+    private String description;
+
+    Category(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}

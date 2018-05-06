e89f9bc87c8699a83e5b117d73151fcc8da0cbf4
seongjun lee <rooneylee@outlook.com>
2018-05-05 22:23:14 -0700

    [INIT] order and customer class

---
+++ src/main/java/pos_delivery/model/Customer.java
@@ -0,0 +1,114 @@
+package pos_delivery.model;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Customer Entity Represent Each Customer Placing An Order.
+ * Customer Has List Of Orders Which Represent Details Of Orders.
+ */
+public class Customer {
+
+    /**
+     * Order Number Is Auto-Generated Unique Number Assigned To Each Customer
+     **/
+    private int orderNumber;
+
+    /**
+     * Name Of Customer
+     **/
+    private String name;
+
+    /**
+     * Date Of Customer Placed Order
+     **/
+    private Date date;
+
+    /**
+     * Time Of Customer Placed Order
+     **/
+    private Time time;
+
+    /**
+     * Orders That The Customer Placed
+     **/
+    private List<Order> orderList;
+
+    /**
+     * Delivery Company That The Customer Used
+     **/
+    private Source source;
+
+    /**
+     * Constructor :
+     * Customer Is Initialized With Name And Source Of Delivery Company.
+     * At The Time of Creation, Date And Time Is Clocked Using System.Time.
+     * For Consistency, First Letter Of Each Word In The Name Is Capitalized.
+     */
+    public Customer() {
+    }
+
+    public Customer(String name, Source source) {
+        long currTime = System.currentTimeMillis();
+        String capitalizedName = Stream.of(name.toLowerCase().trim().split("\\s"))
+                .filter(word -> word.length() > 0)
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+                .collect(Collectors.joining(" "));
+
+        this.name = capitalizedName;
+        this.source = source;
+        this.date = new Date(currTime);
+        this.time = new Time(currTime);
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+}
---
+++ src/main/java/pos_delivery/model/Order.java
@@ -0,0 +1,102 @@
+package pos_delivery.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Order Entity Represents Details Of Each Order.
+ * Order Consists Of Quantity and Menu.
+ * Each Order Can Have Special Instruction Details.
+ */
+public class Order {
+
+    /**
+     * Customer Assoicated With Order
+     **/
+    private Customer customer;
+
+    /**
+     * Menu Associated With Order
+     **/
+    private Menu menu;
+
+    /**
+     * Quantity Of Menu That Customer Ordering
+     **/
+    private int quantity;
+
+    /**
+     * Any Special Instruction On The Order
+     **/
+    private List<String> details;
+
+    /**
+     * Constructor :
+     * Order Requires Customer And Menu.
+     * Each Order Initialized With Empty List Of Special Instruction And Zero Quantity.
+     */
+    public Order() {
+    }
+
+    public Order(Customer customer, Menu menu) {
+        this.customer = customer;
+        this.menu = menu;
+        this.quantity = 0;
+        this.details = new ArrayList<>();
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return menu.equals(order.menu);
+    }
+
+    @Override
+    public int hashCode() {
+        return menu.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        String str = getMenu().getName();
+        for (String detail : details) str += "\n(" + detail + ")";
+
+        return str;
+    }
+}

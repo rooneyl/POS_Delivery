902a26ef9e5aba83da00aac9d5ce00aaa19031b8
seongjun lee <rooneylee@outlook.com>
2018-06-06 20:07:25 -0700

    [FEAT] implement printer job builder

--- src/main/java/pos_delivery/module/PrinterJobBuilder.java
+++ src/main/java/pos_delivery/module/PrinterJobBuilder.java
@@ -1,7 +1,8 @@
 package pos_delivery.module;
 
+import java.io.ByteArrayOutputStream;
+
 public class PrinterJobBuilder {
-    
     private final static String separator = "------------------------";
     private final static byte[] buzzer = new byte[]{0x1B, 0x70, 0x00, (byte) 100, (byte) 250};
     private final static byte[] init = new byte[]{27, 64, 27, 100, 2};
@@ -14,4 +15,59 @@ public class PrinterJobBuilder {
     private final static byte[] regualrSize = new byte[]{29, 33, (byte) 0b00000000};
     private final static byte[] doubleSize = new byte[]{29, 33, (byte) 0b00010001};
 
+
+    private ByteArrayOutputStream byteArray;
+
+    public PrinterJobBuilder() {
+        this.byteArray = new ByteArrayOutputStream();
+        setMode(init, doubleSize, bold_on);
+    }
+
+    public byte[] getByte() {
+        return setMode(cut).getByte();
+    }
+
+    public PrinterJobBuilder fondSize(int size) {
+        return setMode(size == 2 ? doubleSize : regualrSize);
+    }
+
+    public PrinterJobBuilder reverseColor(boolean bool) {
+        return setMode(bool ? reverseColor_on : reverseColor_Off);
+    }
+
+    public PrinterJobBuilder bold(boolean bool) {
+        return setMode(bool ? bold_on : bold_off);
+    }
+
+    public PrinterJobBuilder addSeparator() {
+        return writeString(separator);
+    }
+
+    public PrinterJobBuilder addBuzzer() {
+        return setMode(buzzer, buzzer, buzzer);
+    }
+
+    public PrinterJobBuilder newLine() {
+        return setMode(lineFeed);
+    }
+
+    public PrinterJobBuilder writeString(String... strings) {
+        for (String string : strings) {
+            setMode(string.getBytes(), lineFeed);
+        }
+
+        return this;
+    }
+
+    private PrinterJobBuilder setMode(byte[]... bytes) {
+        try {
+            for (byte[] aByte : bytes) {
+                byteArray.write(aByte);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }
 }

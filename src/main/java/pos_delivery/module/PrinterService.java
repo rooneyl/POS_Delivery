a36924b14a9473e72528c350da8f5f94bf28a8d3
seongjun lee <rooneylee@outlook.com>
2018-06-08 15:23:36 -0700

    [FEAT] basic printer execution

--- src/main/java/pos_delivery/module/PrinterService.java
+++ src/main/java/pos_delivery/module/PrinterService.java
@@ -1,14 +1,55 @@
 package pos_delivery.module;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import pos_delivery.model.Customer;
+
+import javax.print.*;
+import javax.print.attribute.AttributeSet;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.PrinterName;
+import java.util.*;
 
 public class PrinterService {
     static StringBuffer ROLL_PRINTER = new StringBuffer();
     static StringBuffer SASHIMI_PRINTER = new StringBuffer();
     static StringBuffer KITCHEN_PRINTER = new StringBuffer();
     static StringBuffer BILL_PRINTER = new StringBuffer();
+
+    public static void print(Customer customer, List orders) {
+        Map<String, PrinterJobBuilder> printers = new HashMap<>();
+
+        printers.forEach((printer, builder) -> executePrint(printer, builder.getByte()));
+    }
+
+    private static void executePrint(String printer, byte[] b) {
+        try {
+            PrinterName printerName = new PrinterName(printer, null);
+            AttributeSet attrSet = new HashPrintServiceAttributeSet(printerName);
+            DocPrintJob job = PrintServiceLookup.lookupPrintServices(null, attrSet)[0].createPrintJob();
+            DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
+            Doc doc = new SimpleDoc(b, flavor, null);
+            job.print(doc, null);
+        } catch (javax.print.PrintException pex) {
+            System.out.println("Printer Error " + pex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> getPrinters() {
+
+        DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
+        PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
+        PrintService printServices[] = PrintServiceLookup.lookupPrintServices(flavor, pras);
+
+        List<String> printerList = new ArrayList<>();
+        for (PrintService printerService : printServices) {
+            printerList.add(printerService.getName());
+        }
+
+        return printerList;
+    }
 
     public enum Printer {
         ALL(new ArrayList<>(Arrays.asList(SASHIMI_PRINTER, KITCHEN_PRINTER, ROLL_PRINTER))),

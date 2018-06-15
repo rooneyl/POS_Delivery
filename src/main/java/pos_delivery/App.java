e038d393c7a131956e8155c3f360e06662ce5030
seongjun lee <rooneylee@outlook.com>
2018-06-14 19:05:27 -0700

    [FEAT] javafx primayStage

--- src/main/java/pos_delivery/App.java
+++ src/main/java/pos_delivery/App.java
@@ -1,10 +1,49 @@
 package pos_delivery;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import pos_delivery.module.Configurator;
+import pos_delivery.module.DataBaseController;
+
 /**
  * Hello world!
  */
-public class App {
+public class App extends Application {
+    private Scene mainScene;
+    private Stage primaryStage;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("POS_Delivery");
+        primaryStage.setResizable(false);
+
+        FlowPane mainPane = new FlowPane();
+        mainPane.setAlignment(Pos.CENTER);
+        mainScene = new Scene(mainPane, 100,100);
+
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        DataBaseController.getInstance().startDB();
+        Configurator.load();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        DataBaseController.getInstance().closeDataBase();
     }
 }

2c1206dc3d4c9a0ab537eb7a18be2a4baf438cde
seongjun lee <rooneylee@outlook.com>
2018-06-15 19:35:36 -0700

    [FEAT] static constant for application layout

---
+++ src/main/java/pos_delivery/view/Layout.java
@@ -0,0 +1,16 @@
+package pos_delivery.view;
+
+public class Layout {
+    // General
+    public static final double SCREEN_WIDTH = 1024.0;
+    public static final double SCREEN_HEIGHT = 768.0;
+    public static final double SPACING = 10.0;
+
+    public static final String DOORDASH_ICON = "./resources/icons/doordash.png";
+    public static final String FOODORA_ICON = "./resources/icons/foodora.png";
+    public static final String EXIT_ICON = "./resources/icons/exit.png";
+
+    // Primary Scene
+    public static final double COMPANY_BUTTON_SIZE = Layout.SCREEN_WIDTH * 0.15;
+    public static final double SETTING_BUTTON_SIZE = Layout.SCREEN_WIDTH * 0.05;
+}

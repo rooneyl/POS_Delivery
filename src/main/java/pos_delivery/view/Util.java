71edc428ebf70f1f9d58929663ebfa0e8eee64f7
seongjun lee <rooneylee@outlook.com>
2018-06-15 19:36:22 -0700

    [FEAT] util

---
+++ src/main/java/pos_delivery/view/Util.java
@@ -0,0 +1,73 @@
+package pos_delivery.view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.FileInputStream;
+import java.util.Optional;
+
+public class Util {
+
+    public static Button createButton(String imageDir, double size, boolean border) {
+        ImageView imageView;
+        Button button = new Button();
+
+        try {
+            Image image = new Image(new FileInputStream(imageDir));
+            imageView = new ImageView(image);
+        } catch (Exception e) {
+            imageView = new ImageView();
+            border = true;
+        }
+
+        imageView.setFitWidth(size);
+        imageView.setFitHeight(size);
+
+        button.setPrefSize(size, size);
+        button.setGraphic(imageView);
+
+        if (!border)
+            button.setStyle("-fx-background-color: rgba(0,0,0,0)");
+
+        return button;
+    }
+
+    public static VBox getVBox(double width, double height) {
+        VBox vBox = new VBox(Layout.SPACING);
+        vBox.setPrefWidth(width);
+        vBox.setPrefHeight(height);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(Layout.SPACING));
+
+        return vBox;
+    }
+
+    public static HBox getHBox(double width, double height) {
+        HBox hBox = new HBox(Layout.SPACING);
+        hBox.setPrefWidth(width);
+        hBox.setPrefHeight(height);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(Layout.SPACING));
+
+        return hBox;
+    }
+
+    public static Optional<String> getInputText(String title) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle(null);
+        dialog.setGraphic(null);
+        dialog.setHeaderText(title);
+
+        Optional<String> input = dialog.showAndWait();
+        if (input.isPresent() && !input.get().equals(""))
+            return input;
+
+        return Optional.empty();
+    }
+}

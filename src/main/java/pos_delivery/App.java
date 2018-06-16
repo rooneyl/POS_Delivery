a3cc463a12d6ac630e957c3bc98a8bf48b75fe1b
seongjun lee <rooneylee@outlook.com>
2018-06-15 19:37:02 -0700

    [FEAT] mainStage and OrderScene

--- src/main/java/pos_delivery/App.java
+++ src/main/java/pos_delivery/App.java
@@ -3,10 +3,21 @@ package pos_delivery;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import pos_delivery.model.Source;
 import pos_delivery.module.Configurator;
 import pos_delivery.module.DataBaseController;
+import pos_delivery.module.OrderPlacer;
+import pos_delivery.view.Layout;
+import pos_delivery.view.OrderScene;
+import pos_delivery.view.Util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -15,6 +26,8 @@ public class App extends Application {
     private Scene mainScene;
     private Stage primaryStage;
 
+    private OrderScene orderScene;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -22,14 +35,41 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // MainScene, Entry Point of App
+        FlowPane mainPane = new FlowPane();
+        mainPane.setAlignment(Pos.CENTER);
+        mainScene = new Scene(mainPane, Layout.SCREEN_WIDTH,Layout.SCREEN_HEIGHT);
+
+        // Source Selector
+        HBox sourceSelectorBox = Util.getHBox(Layout.SCREEN_WIDTH, Math.floor(Layout.SCREEN_HEIGHT * 0.9));
+        Map<Source, Button> companyButtons = new LinkedHashMap<>();
+        companyButtons.put(Source.DOORDASH, Util.createButton(Layout.DOORDASH_ICON, Layout.COMPANY_BUTTON_SIZE, false));
+        companyButtons.put(Source.FOODORA, Util.createButton(Layout.FOODORA_ICON, Layout.COMPANY_BUTTON_SIZE, false));
+        companyButtons.forEach(((source, button) -> {
+            button.setOnAction(event ->
+                    Util.getInputText("Customer").ifPresent(customer -> {
+                        orderScene = new OrderScene(new AnchorPane(),Layout.SCREEN_WIDTH,Layout.SCREEN_HEIGHT);
+//                        orderScene.setOrderPlacer(new OrderPlacer(customer, source));
+                        primaryStage.setScene(orderScene);
+                    }));
+            sourceSelectorBox.getChildren().add(button);
+        }));
+
+        // Exit Button
+        Button exitButton = Util.createButton(Layout.EXIT_ICON, Layout.SETTING_BUTTON_SIZE, false);
+        exitButton.setOnAction(event -> primaryStage.close());
+
+        // Control Buttons
+        HBox controlButtonBox = Util.getHBox(Layout.SCREEN_WIDTH, Math.floor(Layout.SCREEN_HEIGHT * 0.10));
+        controlButtonBox.setAlignment(Pos.TOP_RIGHT);
+        controlButtonBox.getChildren().addAll(exitButton);
+
+        mainPane.getChildren().addAll(sourceSelectorBox, controlButtonBox);
+
         this.primaryStage = primaryStage;
         primaryStage.setTitle("POS_Delivery");
         primaryStage.setResizable(false);
-
-        FlowPane mainPane = new FlowPane();
-        mainPane.setAlignment(Pos.CENTER);
-        mainScene = new Scene(mainPane, 100,100);
-
+//        primaryStage.setFullScreen(true);
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
---
+++ src/main/java/pos_delivery/view/OrderScene.java
@@ -0,0 +1,10 @@
+package pos_delivery.view;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class OrderScene extends Scene {
+    public OrderScene(Parent root, double width, double height) {
+        super(root, width, height);
+    }
+}

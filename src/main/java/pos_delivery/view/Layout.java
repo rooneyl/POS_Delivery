7783ade3ce53873a1558cc6a5805943f44279541
seongjun lee <rooneylee@outlook.com>
2018-06-16 23:39:34 -0700

    [FEAT] orderScene access buttons

--- src/main/java/pos_delivery/view/Layout.java
+++ src/main/java/pos_delivery/view/Layout.java
@@ -8,9 +8,17 @@ public class Layout {
 
     public static final String DOORDASH_ICON = "./resources/icons/doordash.png";
     public static final String FOODORA_ICON = "./resources/icons/foodora.png";
-    public static final String EXIT_ICON = "./resources/icons/exit.png";
+
 
     // Primary Scene
     public static final double COMPANY_BUTTON_SIZE = Layout.SCREEN_WIDTH * 0.15;
     public static final double SETTING_BUTTON_SIZE = Layout.SCREEN_WIDTH * 0.05;
+
+    // ICONS
+    public static final String RETURN_ICON = "./resources/icons/return.png";
+    public static final String MEMO_ICON = "./resources/icons/memo.png";
+    public static final String DELETE_ICON = "./resources/icons/delete.png";
+    public static final String PRINT_ICON = "./resources/icons/print.png";
+    public static final String SEPARATE_ICON = "./resources/icons/separator.png";
+    public static final String EXIT_ICON = "./resources/icons/exit.png";
 }

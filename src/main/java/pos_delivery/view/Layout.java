e802caf75c22f0d72b5fc78f86d85c60b05f982d
seongjun lee <rooneylee@outlook.com>
2018-06-18 16:19:42 -0700

    [FEAT] javafx style

--- src/main/java/pos_delivery/view/Layout.java
+++ src/main/java/pos_delivery/view/Layout.java
@@ -6,6 +6,10 @@ public class Layout {
     public static final double SCREEN_HEIGHT = 768.0;
     public static final double SPACING = 10.0;
 
+    public static final double ONE_THIRD_WIDTH = Math.floor(SCREEN_WIDTH / 3);
+    public static final double TWO_THIRD_WIDTH = Math.floor(SCREEN_WIDTH - ONE_THIRD_WIDTH);
+
+
     public static final String DOORDASH_ICON = "./resources/icons/doordash.png";
     public static final String FOODORA_ICON = "./resources/icons/foodora.png";
 
@@ -21,4 +25,13 @@ public class Layout {
     public static final String PRINT_ICON = "./resources/icons/print.png";
     public static final String SEPARATE_ICON = "./resources/icons/separator.png";
     public static final String EXIT_ICON = "./resources/icons/exit.png";
+
+    // JavaFx Style
+    public static final String TAB_STYLE = "-fx-font: 16 arial;";
+    public static final String MENU_STYLE = "-fx-font-weight: bold;" +
+            "-fx-border-radius: 5;" +
+            "-fx-background-radius: 5;" +
+            "-fx-border-color: #000000;" +
+            "-fx-border-width: 1px;" +
+            "-fx-background-color: ";
 }

95b638271cb7740264775f539fe5d63d8352c186
seongjun lee <rooneylee@outlook.com>
2018-07-13 17:29:56 -0700

    [INIT] menuLayoutScene init

---
+++ src/main/java/pos_delivery/view/Scene/MenuLayoutScene.java
@@ -0,0 +1,11 @@
+package pos_delivery.view.Scene;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuLayoutScene extends SceneBase {
+
+    public MenuLayoutScene(Stage primaryStage, Scene scene, boolean enableStyle) {
+        super(primaryStage, scene, enableStyle);
+    }
+}
